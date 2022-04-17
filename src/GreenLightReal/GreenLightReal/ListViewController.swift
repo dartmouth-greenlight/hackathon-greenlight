e4481f83adf8b57d2dc11dea08f68d26e948a424
steemend <steven.mendley@gmail.com>
2022-04-17 08:01:24 -0400

    a

--- src/GreenLightReal/GreenLightReal/ListViewController.swift
+++ src/GreenLightReal/GreenLightReal/ListViewController.swift
@@ -7,29 +7,62 @@
 
 import Foundation
 import UIKit
+
 var socialList = ["F004GD3":"Michael Mauricio", "F003XS5":"Steven Mendley"]
 
-func getList(nameList : Dictionary<String,String>) -> String {
-    var list = ""
+
+/*func getList(nameList : Dictionary<String,String>) -> [String] {
+    var list = [String]()
     for id in nameList.keys{
-        list.append("\(nameList[id]) \n")
+        list.append(<#String#>"\(String(describing: nameList[id])))
     
     }
     return list
-}
+}*/
 
 class ListViewController: UIViewController{
-    @IBOutlet var listLabel: UILabel!
-    @IBAction func socialListButton(_ sender: Any) {
-        listLabel.text = getList(nameList: socialList);
-    }
+    @IBOutlet var tableView: UITableView!
     
+    let names = ["Mikey Maurcio", "Steven Mendley"]
+    //@IBAction func OpenTable(_ sender: Any) {
+        
+    //}
     override func viewDidLoad() {
         super.viewDidLoad()
-        listLabel.text = ""
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         
     }
     
     
     
+}
+extension ListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension ListViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
 }
