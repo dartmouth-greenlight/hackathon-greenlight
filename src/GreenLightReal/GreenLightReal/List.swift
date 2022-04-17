981e5a117169f6e21cb14f83bd0015fafc7e65de
Mikey Mauricio <97063493+mikey777mauricio@users.noreply.github.com>
2022-04-17 04:17:09 -0400

    meifne

--- src/GreenLightReal/GreenLightReal/List.swift
+++ src/GreenLightReal/GreenLightReal/List.swift
@@ -1,21 +1,33 @@
 //
-//  ViewController.swift
+//  List.swift
 //  GreenLightReal
 //
 //  Created by Mikey Mauricio on 4/17/22.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class List: UIViewController {
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
    
+    @IBAction func SocialListButton(_ sender: Any) {
+        
+        
+    }
     
 
 
 }
+
+
+
+
 
